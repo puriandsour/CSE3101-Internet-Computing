7dd73ade17e739854752e84d20e01b0eba0f795c
camelo <160200092+Scrapt266@users.noreply.github.com>
2026-01-17 22:13:25 -0400

    Enhance reports overview page layout
    Added a structured layout for the reports overview page, including various depth frames and content sections for EduTrack and teacher reports.

--- school_app/views/teacher/reports/index.php
+++ school_app/views/teacher/reports/index.php
@@ -1,2 +1,150 @@
 <h1>Reports Overview</h1>
-<!-- Figma: figma_export/teachers-reports-screen/index.html -->
+   <div class="main-container">
+        <div class="depth-frame">
+            <div class="depth-frame-1">
+                <div class="depth-frame-2">
+                    <div class="depth-frame-3">
+                        <div class="depth-frame-4"></div>
+                    </div>
+                    <div class="depth-frame-5">
+                        <span class="edutrack">EduTrack</span>
+                    </div>
+                </div>
+                <div class="depth-frame-6">
+                    <div class="depth-frame-7">
+                        <div class="depth-frame-8">
+                            <div class="depth-frame-9">
+                                <div class="vector"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="depth-frame-a"></div>
+                </div>
+            </div>
+            <div class="depth-frame-b">
+                <div class="depth-frame-c">
+                    <div class="depth-frame-d">
+                        <div class="depth-frame-e">
+                            <div class="depth-frame-f">
+                                <div class="depth-frame-10">
+                                    <div class="depth-frame-11">
+                                        <div class="depth-frame-12">
+                                            <span class="edutrack-13">EduTrack</span>
+                                        </div>
+                                        <div class="depth-frame-14">
+                                            <span class="teacher">Teacher</span>
+                                        </div>
+                                    </div>
+                                    <div class="depth-frame-15">
+                                        <div class="depth-frame-16">
+                                            <div class="depth-frame-17">
+                                                <div class="depth-frame-18">
+                                                    <div class="vector-19"></div>
+                                                </div>
+                                            </div>
+                                            <div class="depth-frame-1a">
+                                                <span class="dashboard">Dashboard</span>
+                                            </div>
+                                        </div>
+                                        <div class="depth-frame-1b">
+                                            <div class="depth-frame-1c">
+                                                <div class="depth-frame-1d">
+                                                    <div class="vector-1e"></div>
+                                                </div>
+                                            </div>
+                                            <div class="depth-frame-1f">
+                                                <span class="classes">Classes</span>
+                                            </div>
+                                        </div>
+                                        <div class="depth-frame-20">
+                                            <div class="depth-frame-21">
+                                                <div class="depth-frame-22">
+                                                    <div class="vector-23"></div>
+                                                </div>
+                                            </div>
+                                            <div class="depth-frame-24">
+                                                <span class="enter-scores">Enter Scores</span>
+                                            </div>
+                                        </div>
+                                        <div class="depth-frame-25">
+                                            <div class="depth-frame-26">
+                                                <div class="depth-frame-27">
+                                                    <div class="vector-28"></div>
+                                                </div>
+                                            </div>
+                                            <div class="depth-frame-29">
+                                                <span class="reports">Reports</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="depth-frame-2a">
+                            <div class="depth-frame-2b">
+                                <div class="depth-frame-2c">
+                                    <span class="reports-2d">Reports</span>
+                                </div>
+                                <div class="depth-frame-2e"><span class="slash">/</span></div>
+                                <div class="depth-frame-2f"></div>
+                            </div>
+                            <div class="depth-frame-30">
+                                <div class="depth-frame-31">
+                                    <div class="depth-frame-32">
+                                        <span class="reports-33">Reports</span>
+                                    </div>
+                                </div>
+                                <div class="depth-frame-34">
+                                    <div class="depth-frame-35">
+                                        <div class="depth-frame-36"></div>
+                                        <div class="depth-frame-37">
+                                            <div class="depth-frame-38">
+                                                <span class="grade-performance-analytics">Grade Performance
+                                                    Analytics</span>
+                                            </div>
+                                            <div class="depth-frame-39">
+                                                <div class="depth-frame-3a">
+                                                    <span class="view-insights">View average scores and distribution
+                                                        charts
+                                                        across subjects for a specific grade, providing
+                                                        insights into overall class performance.</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="depth-frame-3b">
+                                            <div class="depth-frame-3c">
+                                                <span class="view-analytics">View Analytics</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="depth-frame-3d">
+                                    <div class="depth-frame-3e">
+                                        <div class="depth-frame-3f"></div>
+                                        <div class="depth-frame-40">
+                                            <div class="depth-frame-41">
+                                                <span class="individual-report">Individual Student Report Card</span>
+                                            </div>
+                                            <div class="depth-frame-42">
+                                                <div class="depth-frame-43">
+                                                    <span class="generate-report">Generate a detailed report card for a
+                                                        specific
+                                                        student, including scores for each subject in a
+                                                        selected term.</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="depth-frame-44">
+                                            <div class="depth-frame-45">
+                                                <span class="select-student">Select Student</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
